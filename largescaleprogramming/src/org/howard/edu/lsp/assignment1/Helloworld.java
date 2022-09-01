c0950a41638f06ee8365375e377d09a7d3bd66c4
ashaanderson <ashaanderson@10.26.98.27>
2022-09-01 18:47:53 -0400

    This is my commit for assignment 1.

---
+++ largescaleprogramming/src/org/howard/edu/lsp/assignment1/Helloworld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Asha Anderson
+ * @author ashaanderson
+ *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
+ */
+
+public class Helloworld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("HellO World");
+
+	}
+
+}
