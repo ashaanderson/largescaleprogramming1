df4e8bea5643c44e3f0f73af310d6e14f5639b8d
ashaanderson <83426123+ashaanderson@users.noreply.github.com>
2022-10-17 20:21:36 -0400

    Integer

---
+++ largescaleprogramming/src/org/howard/edu/lsp/assignment5/IntegerSetException.java
@@ -0,0 +1,13 @@
+package org.howard.edu.assignment5;
+
+public class IntegerSetException extends RuntimeException {
+	
+	/**
+	 * Exception class for IntegerSet
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IntegerSetException(String error) {
+		super(error);
+	}
+}
