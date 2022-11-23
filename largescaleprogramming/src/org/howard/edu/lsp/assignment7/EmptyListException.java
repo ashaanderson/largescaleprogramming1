70dd37c64834c39468b754736ec3e48219f62ff6
ashaanderson <83426123+ashaanderson@users.noreply.github.com>
2022-11-23 17:58:26 -0500

    Create EmptyListException.java

---
+++ largescaleprogramming/src/org/howard/edu/lsp/assignment7/EmptyListException.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment7;
+
+public class EmptyListException extends Exception {
+	  private static final long serialVersionUID = 1L;
+
+	  /**
+	    * This method outputs an error message for the user defined exception
+	    * @param  the  error message
+	    * @return None
+	    */
+	  public EmptyListException(String message) {
+	    super(message);
+	  }
+	}
