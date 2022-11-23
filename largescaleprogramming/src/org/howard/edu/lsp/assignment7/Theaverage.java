4137392059df58b8cc554b3306fb115cbaf50470
ashaanderson <83426123+ashaanderson@users.noreply.github.com>
2022-11-23 18:00:53 -0500

    Create Theaverage.java

---
+++ largescaleprogramming/src/org/howard/edu/lsp/assignment7/Theaverage.java
@@ -0,0 +1,35 @@
+package org.howard.edu.lsp.assignment7;
+
+import java.util.*;
+
+
+
+
+
+/**
+* <h1>Average Context!</h1>
+* <p>The Average Context class implements the Strategy pattern
+* functionality, it enables the program decide what average 
+* computation it uses.
+* </p>
+
+*/
+public class Theaverage {
+  /**
+    * This method sets the average strategy to use
+    * @param AverageStrategy This is the average strategy that's selected
+    * @return 0
+    */
+  public void setAverageStrategy(averagegrade averagegrade){
+  }
+public int computeAverage(List<Integer> listNumbers) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+public void setAverageStrategy(Lowestgrade lowestgrade) {
+	// TODO Auto-generated method stub
+	
+}
+  
+
+}
