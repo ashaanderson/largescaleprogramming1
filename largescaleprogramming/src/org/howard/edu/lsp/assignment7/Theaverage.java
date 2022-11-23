625dd6b460595944601ba70f4429e2867eb8cb69
ashaanderson <83426123+ashaanderson@users.noreply.github.com>
2022-11-23 18:44:05 -0500

    Update Theaverage.java

--- largescaleprogramming/src/org/howard/edu/lsp/assignment7/Theaverage.java
+++ largescaleprogramming/src/org/howard/edu/lsp/assignment7/Theaverage.java
@@ -2,34 +2,37 @@ package org.howard.edu.lsp.assignment7;
 
 import java.util.*;
 
+import org.howard.edu.lsp.assignment7.Strategy.AverageStrategy;
+
 
 
 
 
 /**
-* <h1>Average Context!</h1>
-* <p>The Average Context class implements the Strategy pattern
+* <h1>Average!</h1>
+* <p>The Average  class implements the Strategy pattern
 * functionality, it enables the program decide what average 
 * computation it uses.
 * </p>
 
 */
 public class Theaverage {
-  /**
-    * This method sets the average strategy to use
-    * @param AverageStrategy This is the average strategy that's selected
-    * @return 0
-    */
-  public void setAverageStrategy(averagegrade averagegrade){
-  }
-public int computeAverage(List<Integer> listNumbers) {
-	// TODO Auto-generated method stub
-	return 0;
-}
-public void setAverageStrategy(Lowestgrade lowestgrade) {
-	// TODO Auto-generated method stub
-	
-}
-  
-
-}
+	  private AverageStrategy strategy;
+	  /**
+	    * This method sets the average strategy to use
+	    * @param AverageStrategy This is the average strategy that's selected
+	    * @return None
+	    */
+	  public void setAverageStrategy(AverageStrategy strategy){
+	    this.strategy = strategy;
+	  }
+	  
+	  /**
+	    * This method computes the average using the selected strategy
+	    * @param grades This is a list of grades
+	    * @return int This returns the desired average
+	    */
+	  public int computeAverage(List<Integer> grades) throws EmptyListException{
+	    return strategy.compute(grades);
+	  }
+	}
